003fcea56487123b1cce582861d2315112ad9a0a
wayttyyyy <dimagoldinskij@gmail.com>
2026-05-22 14:40:53 +0300

    feat: add main view to display 5 latest recipes

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,7 @@
+from django.shortcuts import render
+from .models import Recipe, Category
+from django.db.models import Count
 
+def main(request):
+    latest_recipes = Recipe.objects.order_by('-created_at')[:5] 
+    return render(request, 'main.html', {'recipes': latest_recipes})
