0986b5f8f262801254a03ccf4a97d35ddbb2f14a
wayttyyyy <dimagoldinskij@gmail.com>
2026-05-22 14:50:13 +0300

    test: add unit test for category_list view

--- recipe/tests.py
+++ recipe/tests.py
@@ -17,4 +17,9 @@ class RecipeViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main.html')
         self.assertIn('recipes', response.context)
-        
+
+    def test_category_list_view(self):
+        response = self.client.get(reverse('category_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'category_list.html')
+        self.assertIn('categories', response.context)
