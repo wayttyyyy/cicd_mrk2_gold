a26c3ce5a60f71d47f46baf4c25ce1ca1bab8cdd
wayttyyyy <dimagoldinskij@gmail.com>
2026-05-22 14:48:10 +0300

    test: add unit test for main view

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Category, Recipe
 
-# Create your tests here.
+
+class RecipeViewsTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Desserts")
+        Recipe.objects.create(
+            title="Cake", description="Yummy", 
+            instructions="Bake", ingredients="Flour", 
+            category=self.category
+        )
+
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
+        self.assertIn('recipes', response.context)
+        
